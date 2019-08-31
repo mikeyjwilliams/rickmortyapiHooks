0470465c862132f6c228f67e031457aee3696a83
Michael Williams <manicdesigner@gmail.com>
2019-08-31 19:23:37 -0400

    add RickMortyComponent to dir

---
+++ src/components/RickMortyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function RickMortyComponent(props) {
+
+    return (
+        <>
+            <p>Rick &amp; Morty</p>
+
+        </>
+    )
+
+}
+export default RickMortyComponent;
