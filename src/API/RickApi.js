7b98685c8996dea901154b88a0a8b6454c1dcadf
Michael Williams <manicdesigner@gmail.com>
2019-08-31 19:07:06 -0400

    add setter and page parameters, avail for placeholders and comments for components

--- src/API/RickApi.js
+++ src/API/RickApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export function RickApi() {
+export function RickApi(setter, page) {
     axios.get('https://rickandmortyapi.com/api/character/?page=1')
         .then(res => console.log(res))
         .catch(err => console.log(err))
