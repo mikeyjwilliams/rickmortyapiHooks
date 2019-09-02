e84e1410ac8a221aeda61b429ca4a00874fc85c4
Michael Williams <manicdesigner@gmail.com>
2019-09-01 20:56:29 -0400

    styles components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState, useEffect } from 'react';
 import {RickApi} from './API/RickApi';
 import {RickMortyTitle }  from './components/RickMortyTitle';
--- src/components/RickMortyComponent.js
+++ src/components/RickMortyComponent.js
@@ -16,20 +16,24 @@ function RickMortyComponent(props) {
         //   
             <Container>
                 <RowCustom>
+                
                             {rickMortyCharactersData.map(rickMortyCharacter => {
                                 return (
-                                    <Col xs="12" sm="12" md="6" lg="3" >
-                                    <CardColumnsCustom key={rickMortyCharacter.id + 'custom'}>
+                                    <Col md={6} lg={4} xl={3}>
+                                    <CardCustom key={rickMortyCharacter.id + 'custom'}>
                                     <CharacterCard key={rickMortyCharacter.id} >
                                         <RickMortyImage rickMortyCharacter={rickMortyCharacter} key={rickMortyCharacter.id + 'c'} />
                                         <RickMortyName rickMortyCharacter={rickMortyCharacter} key={rickMortyCharacter.id + 'n'} />
                                         <RickMortyLocation rickMortyCharacter={rickMortyCharacter} key={rickMortyCharacter.id + 'l'} />
                                         <RickMortyStatus rickMortyCharacter={rickMortyCharacter} key={rickMortyCharacter.id + 's'} />
                                     </CharacterCard>
-                                    </CardColumnsCustom>
+            
+                                    </CardCustom>
                                     </Col>
+                                
                                 )
                             })}
+                              
                 </RowCustom>  
             </Container>
       
@@ -38,18 +42,21 @@ function RickMortyComponent(props) {
 }
 export default RickMortyComponent;
 
-const CardColumnsCustom = styled(CardColumns)`
-	/* display: flex;
-	flex-wrap: wrap;
-	justify-content: space-around; */
+const CardCustom = styled(Card)`
+	background-color: rgba(226, 106, 153, 0.19);
+	width: 100%;
+    display: block;
+    border: 1px solid red;
 `;
 
 const RowCustom = styled(Row)`
-    display: flex;
-    flex-wrap: wrap;
+    /* display: flex;
+    flex-wrap: wrap; */
 `;
 
 const CharacterCard = styled(Card)`
 	background-color: rgba(226, 106, 153, 0.19);
-	width: 25%;
+	width: 100%;
+    display: block;
+    border: 1px solid red;
 `;
